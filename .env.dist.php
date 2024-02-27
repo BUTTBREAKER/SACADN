751512bd3dcb34049a0cdb3e2fe573f537daae9f
fadrian06 <franyeradriansanchez@gmail.com>
2024-02-26 21:07:24 -0400

    Added Env vars config file

---
+++ .env.dist.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  "DB_CONNECTION" => "mysql",
+  // "DB_CONNECTION" => "sqlite",
+  "DB_HOST" => "localhost",
+  "DB_PORT" => 3306,
+  "DB_DATABASE" => "sacadn", # <-- mysql
+  // "DB_DATABASE" => __DIR__ . "/app/database/sacadn.db", # <-- sqlite
+  "DB_USERNAME" => "root",
+  "DB_PASSWORD" => ""
+];
