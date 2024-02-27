73a1f22efb656286c9ca8a99ca1741635b568966
fadrian06 <franyeradriansanchez@gmail.com>
2024-02-27 13:01:42 -0400

    Applied local font

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>SACADN</title>
     <link rel="icon" type="image/ico" href="./Sacadn.ico"/>
+    <link rel="stylesheet" href="./Assets/fonts.css" />
     <link rel="stylesheet" href="./Assets/style.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 </head>
