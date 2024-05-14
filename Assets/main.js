709d97653f900837cc3a23f5d7f6a59fb4cd53f6
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-14 14:28:55 -0400

    Fixed JS error when register button doesn't exists

--- Assets/main.js
+++ Assets/main.js
@@ -3,9 +3,11 @@ const registerButton = document.getElementById("register");
 const loginButton = document.getElementById("login");
 const container = document.getElementById("container");
 
-registerButton.addEventListener("click", () => {
-    container.classList.add("right-panel-active");
-});
+if (registerButton) {
+  registerButton.addEventListener("click", () => {
+      container.classList.add("right-panel-active");
+  });
+}
 
 loginButton.addEventListener("click", () => {
     container.classList.remove("right-panel-active");
