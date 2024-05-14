9ed2cef9b2f627b17dec5bf3a245c056c432bfac
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-14 14:47:39 -0400

    Added DI Container dependency

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+use Illuminate\Container\Container;
+use Psr\Container\ContainerInterface;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$_ENV += require __DIR__ . '/../.env.php';
+
+date_default_timezone_set('America/Caracas');
+
 function updateErrorsUrls(string $htaccessPath, string $errorsDir): void {
   $root = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
 
@@ -11,4 +19,28 @@ function updateErrorsUrls(string $htaccessPath, string $errorsDir): void {
   htaccess;
 
   file_put_contents($htaccessPath, $errors);
+}
+
+function container(): ContainerInterface {
+  static $container = null;
+
+  if (!$container) {
+    $container = new Container();
+
+    $container->bind(mysqli::class, function (): mysqli {
+      $mysql = new mysqli(
+        $_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']
+      );
+
+      try {
+        $mysql->select_db($_ENV['DB_DATABASE']);
+      } catch (mysqli_sql_exception) {
+        $mysql->query('CREATE DATABASE ' . $_ENV['DB_DATABASE']);
+      }
+
+      return $mysql;
+    }, true);
+  }
+
+  return $container;
 }
