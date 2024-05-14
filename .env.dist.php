13c44a809a9fc1c00a81cfe3ee60ce1f371478a3
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-13 23:59:14 -0400

    Database folder moved to root

--- .env.dist.php
+++ .env.dist.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-  "DB_CONNECTION" => "mysql",
+  'DB_CONNECTION' => 'mysql',
   // "DB_CONNECTION" => "sqlite",
-  "DB_HOST" => "localhost",
-  "DB_PORT" => 3306,
-  "DB_DATABASE" => "sacadn1", # <-- mysql
-  // "DB_DATABASE" => __DIR__ . "/app/database/sacadn.db", # <-- sqlite
-  "DB_USERNAME" => "root",
-  "DB_PASSWORD" => ""
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => 3306,
+  'DB_DATABASE' => 'sacadn1', # <-- mysql
+  // "DB_DATABASE" => __DIR__ . "/database/sacadn.db", # <-- sqlite
+  'DB_USERNAME' => 'root',
+  'DB_PASSWORD' => ''
 ];
--- app/commands/db-install.php
+++ app/commands/db-install.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 $_ENV += require __DIR__ . '/../../.env.php';
 
 switch (strtolower($_ENV['DB_CONNECTION'])) {
@@ -8,16 +10,22 @@ switch (strtolower($_ENV['DB_CONNECTION'])) {
     break;
 
   case 'mysql':
-    $mysql = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+    $mysql = new mysqli(
+      $_ENV['DB_HOST'],
+      $_ENV['DB_USERNAME'],
+      $_ENV['DB_PASSWORD']
+    );
 
     try {
       $mysql->select_db($_ENV['DB_DATABASE']);
     } catch (mysqli_sql_exception) {
-      $mysql->query("CREATE DATABASE " . $_ENV['DB_DATABASE']);
+      $mysql->query('CREATE DATABASE ' . $_ENV['DB_DATABASE']);
     }
 
     $mysql->close();
-    $dsn = "mysql:host={$_ENV['DB_HOST']}; dbname={$_ENV['DB_DATABASE']}; charset=utf8";
+    $dsn = "mysql:host={$_ENV['DB_HOST']};";
+    $dsn .= "dbname={$_ENV['DB_DATABASE']};";
+    $dsn .= "charset=utf8";
 
     try {
       $pdo = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
@@ -31,7 +39,8 @@ switch (strtolower($_ENV['DB_CONNECTION'])) {
     exit("❌ DB_CONNECTION \"{$_ENV['DB_CONNECTION']}\" not supported!");
 }
 
-$sqlFilePath = dirname(__DIR__) . "/database/init.{$_ENV['DB_CONNECTION']}.sql";
+$sqlFilePath = dirname(__DIR__, 2)
+  . "/database/init.{$_ENV['DB_CONNECTION']}.sql";
 
 foreach (explode(';', file_get_contents($sqlFilePath)) as $statement) {
   $statement = trim($statement);
@@ -40,7 +49,11 @@ foreach (explode(';', file_get_contents($sqlFilePath)) as $statement) {
     continue;
   }
 
-  $pdo->query($statement);
+  try {
+    $pdo->query($statement);
+  } catch (PDOException $error) {
+    file_put_contents('php://stderr', $error->getMessage() . PHP_EOL);
+  }
 }
 
 echo "✔️ DB \"{$_ENV['DB_DATABASE']}\" installed correctly!";
