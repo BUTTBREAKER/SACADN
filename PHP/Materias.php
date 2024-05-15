7a6fb6817849099ce635d96eadf03424c29cdf38
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-15 19:01:18 -0400

    Solving some merge conflicts and solvind no white box on page content

--- PHP/Materias.php
+++ PHP/Materias.php
@@ -6,50 +6,35 @@ require __DIR__ . '/../vendor/autoload.php';
 $db = require_once __DIR__ . '/conexion_be.php';
 include __DIR__ . '/partials/header.php';
 
-$sql = 'SELECT ID_mater as id, codigo, nombre, fech_mater as fecha_registro FROM materias';
+$sql = 'SELECT ID_mater as id, codigo, nombre, fech_mater as fecha_registro
+FROM materias';
 
 $result = $db->query($sql);
 
 ?>
 
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>Lista de materias</title>
-
-  <link rel="stylesheet" href="../Assets/simple-datatables/simple-datatables.css">
-</head>
-
-<body>
-=======
->>>>>>> 9eb4aa044c6c570aba6ac708f44cb0a9dc53f618
-  <div style="overflow-x: auto;">
-    <table id="tablaMaterias" class="datatable">
+<div class="container" style="overflow-x: auto;">
+  <table id="tablaMaterias" class="datatable">
+    <tr>
+      <th>ID</th>
+      <th>Código</th>
+      <th>Nombre</th>
+      <th>Fecha de registro</th>
+    </tr>
+    <?php while ($materia = $result->fetch_assoc()) { ?>
       <tr>
-        <th>ID</th>
-        <th>Código</th>
-        <th>Nombre</th>
-        <th>Fecha de registro</th>
+        <td><?= $materia['id'] ?></td>
+        <td><?= substr(strtoupper($materia['nombre']), 0, 3) . '-' . $materia['codigo'] ?></td>
+        <td><?= $materia['nombre'] ?></td>
+        <td><?= formatearFecha($materia['fecha_registro']) ?></td>
       </tr>
-      <?php while ($materia = $result->fetch_assoc()) { ?>
-        <tr>
-          <td><?= $materia['id'] ?></td>
-          <td><?= substr(strtoupper($materia['nombre']), 0, 3) . '-' . $materia['codigo'] ?></td>
-          <td><?= $materia['nombre'] ?></td>
-          <td><?= formatearFecha($materia['fecha_registro']) ?></td>
-        </tr>
-      <?php } ?>
-    </table>
-  </div>
+    <?php } ?>
+  </table>
+</div>
 
-  <script src="../Assets/simple-datatables/simple-datatables.min.js"></script>
-  <script>
-    const tablaMaterias = new simpleDatatables.DataTable("#tablaMaterias");
-  </script>
+<script src="../Assets/simple-datatables/simple-datatables.min.js"></script>
+<script>
+  const tablaMaterias = new simpleDatatables.DataTable("#tablaMaterias");
+</script>
 
 <?php include __DIR__ . '/partials/footer.php' ?>
--- PHP/Representantes.php
+++ PHP/Representantes.php
@@ -17,58 +17,58 @@ $result = $db->query($sql);
 
 ?>
 
-  <div style="overflow-x: auto;">
-    <table id="tablaRepresentantes" class="datatable">
-      <thead>
-        <tr>
-          <!-- <td>ID</td> -->
-          <th>Cédula</th>
-          <th>Nombres</th>
-          <th>Apellidos</th>
-          <th>Fecha de nacimiento</th>
-          <th>Edad</th>
-          <th>Estado de nacimiento</th>
-          <th>Lugar de nacimiento</th>
-          <th>Sexo</th>
-          <th>Teléfono</th>
-          <th>Dirección</th>
-          <th>Fecha</th>
-          <th>Opciones</th>
-        </tr>
-      </thead>
-      <tbody>
+<div class="container" style="overflow-x: auto;">
+  <table id="tablaRepresentantes" class="datatable">
+    <thead>
+      <tr>
+        <!-- <td>ID</td> -->
+        <th>Cédula</th>
+        <th>Nombres</th>
+        <th>Apellidos</th>
+        <th>Fecha de nacimiento</th>
+        <th>Edad</th>
+        <th>Estado de nacimiento</th>
+        <th>Lugar de nacimiento</th>
+        <th>Sexo</th>
+        <th>Teléfono</th>
+        <th>Dirección</th>
+        <th>Fecha</th>
+        <th>Opciones</th>
+      </tr>
+    </thead>
+    <tbody>
       <?php while ($mostrar = $result->fetch_assoc()) { ?>
-          <tr>
-            <td><?= $mostrar['cedula'] ?></td>
-            <td><?= $mostrar['nombres'] ?></td>
-            <td><?= $mostrar['apellidos'] ?></td>
-            <td><?= formatearFecha($mostrar['fecha_nacimiento']) ?></td>
-            <td><?= calcularEdad($mostrar['fecha_nacimiento']) ?></td>
-            <td><?= $mostrar['estado_nacimiento'] ?></td>
-            <td><?= $mostrar['lugar_nacimiento'] ?></td>
-            <td><?= $mostrar['sexo'] ?></td>
-            <td><?= $mostrar['telefono'] ?></td>
-            <td><?= $mostrar['direccion'] ?></td>
-            <td><?= formatearFecha($mostrar['fecha_registro']) ?></td>
-            <td>
-              <form method="post">
-                <button formaction="eliminar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
-                  Eliminar
-                </button>
-                <button formaction="editar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
-                  Editar
-                </button>
-              </form>
-            </td>
-          </tr>
-          <?php } ?>
-        </tbody>
-    </table>
-  </div>
+        <tr>
+          <td><?= $mostrar['cedula'] ?></td>
+          <td><?= $mostrar['nombres'] ?></td>
+          <td><?= $mostrar['apellidos'] ?></td>
+          <td><?= formatearFecha($mostrar['fecha_nacimiento']) ?></td>
+          <td><?= calcularEdad($mostrar['fecha_nacimiento']) ?></td>
+          <td><?= $mostrar['estado_nacimiento'] ?></td>
+          <td><?= $mostrar['lugar_nacimiento'] ?></td>
+          <td><?= $mostrar['sexo'] ?></td>
+          <td><?= $mostrar['telefono'] ?></td>
+          <td><?= $mostrar['direccion'] ?></td>
+          <td><?= formatearFecha($mostrar['fecha_registro']) ?></td>
+          <td>
+            <form method="post">
+              <button formaction="eliminar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
+                Eliminar
+              </button>
+              <button formaction="editar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
+                Editar
+              </button>
+            </form>
+          </td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</div>
 
-  <script src="../Assets/simple-datatables/simple-datatables.min.js"></script>
-  <script>
-    const tablaRepresentantes = new simpleDatatables.DataTable("#tablaRepresentantes");
-  </script>
+<script src="../Assets/simple-datatables/simple-datatables.min.js"></script>
+<script>
+  const tablaRepresentantes = new simpleDatatables.DataTable("#tablaRepresentantes");
+</script>
 
 <?php include __DIR__ . '/partials/footer.php' ?>
