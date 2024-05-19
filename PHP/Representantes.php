d2520fff01e64a811cb5b8e4d083e7216a063b45
Jose Acevedo <149984522+BUTTBREAKER@users.noreply.github.com>
2024-05-19 18:20:08 -0400

    agregar estilos de editar y actualizar en tablas

--- PHP/Representantes.php
+++ PHP/Representantes.php
@@ -51,13 +51,15 @@ $result = $db->query($sql);
           <td><?= $mostrar['direccion'] ?></td>
           <td><?= formatearFecha($mostrar['fecha_registro']) ?></td>
           <td>
-            <form method="post">
-              <button formaction="eliminar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
-                Eliminar
-              </button>
-              <button formaction="editar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
-                Editar
-              </button>
+            <form method="post" class="botones-tabla">
+              <button class="eliminar" formaction="eliminar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
+<i>              <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"><path fill="#ffffff" d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zM17 6H7v13h10zM7 6v13zm5 7.9l1.9 1.9q.275.275.7.275t.7-.275t.275-.7t-.275-.7l-1.9-1.9l1.9-1.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275L12 11.1l-1.9-1.9q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l1.9 1.9l-1.9 1.9q-.275.275-.275.7t.275.7t.7.275t.7-.275z"/></svg>
+ </button>
+</i>
+              <button class="editar" formaction="editar-representante.php?cedula=<?= $mostrar['cedula'] ?>">
+<i>              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14"><g fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round"><path d="m7.5 9l-3 .54L5 6.5L10.73.79a1 1 0 0 1 1.42 0l1.06 1.06a1 1 0 0 1 0 1.42Z"/><path d="M12 9.5v3a1 1 0 0 1-1 1H1.5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3"/></g></svg>
+</i>
+            </button>
             </form>
           </td>
         </tr>
