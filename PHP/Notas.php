a80510a3a2ec27fdb458445c5aaefccbbfcbbddf
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-25 01:24:23 -0400

    Fix notas.php

--- PHP/Notas.php
+++ PHP/Notas.php
@@ -1,16 +1,15 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-// Incluir el archivo de conexión a la base de datos
+
 /** @var mysqli */
 $db = require_once __DIR__ . '/conexion_be.php';
 include __DIR__ . '/partials/header.php';
 
-/* Selecciona campo ci_repr y cambiale el nombre a cedula, ..., de la tabla notas */
 $sql = <<<SQL
-  SELECT ci_repr AS cedula, nombre_completo AS nombres, apellido AS apellidos,
-  fecha_nac AS fecha_nacimiento, estado as estado_nacimiento, lugar AS lugar_nacimiento,
-  genero AS sexo, telefono, direccion, fech_repr AS fecha_registro FROM representantes
+  SELECT cedula, nombre as nombres, apellido AS apellidos,
+  fecha_nacimiento, lugar_nacimiento, genero AS sexo, telefono, direccion,
+  fecha_registro FROM representantes
 SQL;
 
 $result = $db->query($sql);
