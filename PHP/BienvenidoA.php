d8566b558f49e07aa9f80a5e591c9e872fa70c02
fadrian06 <franyeradriansanchez@gmail.com>
2024-05-26 20:14:46 -0400

    Added chart.js

--- PHP/BienvenidoA.php
+++ PHP/BienvenidoA.php
@@ -11,6 +11,31 @@ $user = auth()['usuario'];
 <div class="container card card-body">
   <h1>Bienvenido, <?= $user ?></h1>
   <p>¡Gracias por iniciar sesión!</p>
+  <canvas id="myChart" style="width: 100%"></canvas>
 </div>
+
+<script>
+  const ctx = document.getElementById('myChart')
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+        backgroundColor: ['red', 'blue', 'yellow', 'green', 'purple', 'orange']
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  })
+</script>
 
 <?php include __DIR__ . '/partials/footer.php' ?>
--- PHP/partials/header.php
+++ PHP/partials/header.php
@@ -31,6 +31,8 @@ try {
   <link rel="stylesheet" href="../vendor/thomaspark/bootswatch/dist/sandstone/bootstrap.min.css" />
   <link rel="stylesheet" href="../vendor/select2/select2/dist/css/select2.min.css" />
   <link rel="stylesheet" href="../vendor/apalfrey/select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css" />
+  <script src="../Assets/ResizeObserver.global.js"></script>
+  <script src="../Assets/chart.js"></script>
 </head>
 
 <body>
