ef178583473f52feb0bad21d866924ba7d439b1a
ElaRondon <elainerondon2004@gmail.com>
2024-06-27 18:37:22 -0430

    editar usuarios

--- PHP/editar-representante.php
+++ PHP/editar-representante.php
@@ -143,12 +143,6 @@ $representante = $stmt->get_result()->fetch_assoc();
     </div>
   </form>
 </div>
-            <!--<div class="contenedor1">
-              <i><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
-                  <path fill="#000000" d="M12 19.35q3.05-2.8 4.525-5.087T18 10.2q0-2.725-1.737-4.462T12 4Q9.475 4 7.738 5.738T6 10.2q0 1.775 1.475 4.063T12 19.35m0 1.975q-.35 0-.7-.125t-.625-.375Q9.05 19.325 7.8 17.9t-2.087-2.762q-.838-1.338-1.275-2.575T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 1.125-.437 2.363t-1.275 2.575Q17.45 16.475 16.2 17.9t-2.875 2.925q-.275.25-.625.375t-.7.125M12 12q.825 0 1.413-.587T14 10q0-.825-.587-1.412T12 8q-.825 0-1.412.588T10 10q0 .825.588 1.413T12 12" />
-                </svg></i>
-              <label for="edo_repr">Estado</label>
-              <input type="text" value="<?= $representante['estado_nacimiento'] ?>" id="registerEstado" placeholder="Estado" name="estado" required>
-            </div>-->
+          
 
 <?php include __DIR__ . '/partials/footer.php' ?>
--- PHP/editar-usuario.php
+++ PHP/editar-usuario.php
@@ -3,18 +3,19 @@
 include __DIR__ . '/partials/header.php';
 require __DIR__ . "/Middlewares/autorizacion.php";
 // Incluir archivo de conexión a la base de datos
-require_once __DIR__ . '/conexion_be.php';
+
+$db = require_once __DIR__ . '/conexion_be.php';
+
 if ($_POST) {
   $sql = <<<SQL
  UPDATE usuarios SET
  ID = ?,
  nombre = ?,
  apellido = ?,
- Cedula = ?,
- Usuarios = ?,
+ cedula = ?,
+ usuario = ?,
  clave = ?,
- rol = ?,
- estado = ?
+ rol = ?
  WHERE ID= ?
  SQL;
 
@@ -23,13 +24,11 @@ $db->prepare($sql)
 $_POST['ID'],
 $_POST['nombre'],
 $_POST['apellido'],
-$_POST['Cedula'],
-$_POST['Usuarios'],
+$_POST['cedula'],
+$_POST['usuario'],
 $_POST['clave'],
-$_POST['genero'],
 $_POST['rol'],
-$_POST['estado'],
-$_POST['cedula']
+$_POST['ID']
 ]);
 
  exit(<<<HTML
@@ -38,47 +37,50 @@ $_POST['cedula']
     <script src="../assets/sweetalert2/sweetalert2.min.js"></script>
     <script>
       Swal.fire({
-        title: 'Profesor actualizado correctamente',
+        title: 'Usuario actualizado correctamente',
         icon: 'success',
         showConfirmButton: false,
         timer: 3000
-      }).then(() => location.href = './Profesores.php' )
+      }).then(() => location.href = './ModuloA.php' )
     </script>
   </body>
   HTML);
 
 }
 
-$sql = "SELECT ID, nombre, apellido, Cedula, Usuario, clave, rol, estado FROM usuarios";
+$sql = "SELECT ID, nombre, apellido, cedula, usuario, clave, rol FROM usuarios";
 $resultado = $conexion->query($sql);
+$usuario = $resultado->fetch_assoc()
 ?>
 
 <div class=" row mx-0 justify-content-center pb-5">
 <form class="card col-md-7 py-4 mt-3" action="registro_usuario_be.php" method="POST">
-  <h2 class="card-title h3 text-center">Crear Nuevo Usuario</h2>
+  <h2 class="card-title h3 text-center">Editar Usuario</h2>
   <div class="card-body row">
     <div class="form-floating mb-2 col-6">
-      <input type="text" class="form-control form-control-sm" id="registerUsuario" placeholder="Usuario" name="usuario" required pattern="[A-Za-z0-9]+" maxlength="20" title="El usuario solo puede contener letras y números, con un máximo de 20 caracteres." />
+      <input type="text" class="form-control form-control-sm" value="<?= $usuario['usuario'] ?>" id="registerUsuario" placeholder="Usuario" name="usuario" required pattern="[A-Za-z0-9]+" maxlength="20" title="El usuario solo puede contener letras y números, con un máximo de 20 caracteres." />
       <label class="ms-2" for="registerUsuario">Usuario</label>
     </div>
+
     <div class="form-floating mb-2 col-6">
-      <input type="text" class="form-control form-control-sm" id="registerNombreCompleto" placeholder="Nombre" name="nombre" required maxlength="50" onkeypress="return soloLetrasYEspacios(event)" title="El nombre solo puede contener letras y espacios, con un máximo de 50 caracteres." />
+       <div class="form-control form-control-sm" placeholder=" " id="registerNombreCompleto" name="id_representante" required ><?php echo $usuario['nombre']; ?> </div>
+     
       <label class="ms-2" for="registerNombreCompleto">Nombres</label>
     </div>
     <div class="form-floating mb-2 col-6">
-      <input type="text" class="form-control form-control-sm" id="registerApellidoCompleto" placeholder="Apellido" name="apellido" required maxlength="50" onkeypress="return soloLetrasYEspacios(event)" title="El apellido solo puede contener letras y espacios, con un máximo de 50 caracteres." />
+      <input type="text" class="form-control form-control-sm" value="<?= $usuario['apellido'] ?>" id="registerApellidoCompleto" placeholder="Apellido" name="apellido" required maxlength="50" onkeypress="return soloLetrasYEspacios(event)" title="El apellido solo puede contener letras y espacios, con un máximo de 50 caracteres." />
       <label class="ms-2" for="registerApellidoCompleto">Apellidos</label>
     </div>
     <div class="form-floating mb-2 col-6">
-      <input type="number" class="form-control form-control-sm" id="registerCedula" placeholder="Cédula" name="cedula" required min="0" pattern="[0-9]{11}" title="La cédula debe contener 11 dígitos numéricos." />
+      <input type="number" class="form-control form-control-sm" value="<?= $usuario['cedula'] ?>" id="registerCedula" placeholder="Cédula" name="cedula" required min="0" pattern="[0-9]{11}" title="La cédula debe contener 11 dígitos numéricos." />
       <label class="ms-2" for="registerCedula">Cédula</label>
     </div>
     <div class="form-floating mb-2 col-6">
-      <input type="password" class="form-control form-control-sm" id="registerContrasena" placeholder="Contraseña" name="clave" required minlength="4" maxlength="20" pattern=".{4,20}" title="La contraseña debe tener entre 4 y 20 caracteres." />
+      <input type="password" class="form-control form-control-sm"  id="registerContrasena" placeholder="Contraseña" name="clave" required minlength="4" maxlength="20" pattern=".{4,20}" title="La contraseña debe tener entre 4 y 20 caracteres." />
       <label class="ms-2" for="registerContrasena">Contraseña</label>
     </div>
     <div class="form-floating mb-2 col-6">
-      <select class="form-control form-control-sm" id="rol" name="rol" required>
+      <select class="form-control form-control-sm" value="<?= $usuario['rol'] ?>" id="rol" name="rol" required>
         <option value="A">Administrador</option>
         <option value="U">Usuario</option>
       </select>
@@ -91,42 +93,4 @@ $resultado = $conexion->query($sql);
 </form>
 </div>
 
-
-<script src="../Assets/simple-datatables/simple-datatables.min.js"></script>
-<script>
-document.addEventListener('DOMContentLoaded', () => {
-  const table = new simpleDatatables.DataTable('#userTable');
-});
-</script>
-<script>
-function soloLetrasYEspacios(event) {
-  const tecla = event.key;
-  const esLetra = (tecla >= 'a' && tecla <= 'z') || (tecla >= 'A' && tecla <= 'Z');
-  const esEspacio = tecla === ' ';
-
-  // Si la tecla no es una letra ni un espacio, se cancela la acción predeterminada
-  if (!esLetra && !esEspacio) {
-    event.preventDefault();
-    return false;
-  }
-}
-
-
-document.querySelectorAll('[data-action]').forEach(function(link) {
-  link.onclick = function(event) {
-    event.preventDefault()
-
-    const url = './alternar-estado.php?toggle_estado=true&usuario_id=' + link.dataset.userId + '&nuevo_estado=' + link.dataset.newState
-
-    console.log(url)
-    fetch(url)
-      .then(function() {
-        link.innerText = link.innerText === 'Activar' ? 'Desactivar' : 'Activar'
-        link.parentElement.previousElementSibling.innerText = link.parentElement.previousElementSibling.innerText === 'Activo' ? 'Inactivo' : 'Activo'
-      })
-  }
-})
-</script>
-
-<?php include __DIR__ . '/partials/footer.php' ?>
 <?php include __DIR__ . '/partials/footer.php' ?>
